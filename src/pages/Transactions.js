64366074eda3e82becd26aacbd03a7e1903568eb
Tony Eder <tonyeder11@gmail.com>
2024-01-11 13:56:58 -0800

    transactions static page drafted

--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -4,7 +4,62 @@ function Transactions() {
     return (
         <>
             <main>
-                <h1>Table showing list of transactions</h1>
+                <h1>Where did all the money go?</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>DATE</th>
+                            <th>CATEGORY</th>
+                            <th>DESCRIPTION</th>
+                            <th>AMOUNT</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1/1/2024</td>
+                            <td>Income</td>
+                            <td>Paycheck</td>
+                            <td>$2000</td>
+                        </tr>
+                        <tr>
+                            <td>1/7/2024</td>
+                            <td>Expense</td>
+                            <td>Home</td>
+                            <td>$1700</td>
+                        </tr>
+                        <tr>
+                            <td>1/1/2024</td>
+                            <td>Income</td>
+                            <td>Paycheck</td>
+                            <td>$2000</td>
+                        </tr>
+                        <tr>
+                            <td>1/1/2024</td>
+                            <td>Income</td>
+                            <td>Paycheck</td>
+                            <td>$2000</td>
+                        </tr>
+                        <tr>
+                            <td>1/1/2024</td>
+                            <td>Income</td>
+                            <td>Paycheck</td>
+                            <td>$2000</td>
+                        </tr>
+                        <tr>
+                            <td>1/1/2024</td>
+                            <td>Income</td>
+                            <td>Paycheck</td>
+                            <td>$2000</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table>
+                    <tr>
+                        <th>TOTAL SPEND: </th>
+                    </tr>
+                    <td>$11,700
+                    </td>
+                </table>
             </main>
         </>
     )
