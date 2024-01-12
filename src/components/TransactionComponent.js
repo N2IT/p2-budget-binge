5a278699fe34d2174c87d60edf15e2bb9c176e52
Tony Eder <tonyeder11@gmail.com>
2024-01-12 11:24:14 -0800

    transaction page / component rolling

--- src/components/TransactionComponent.js
+++ src/components/TransactionComponent.js
@@ -2,69 +2,34 @@ import { useOutletContext } from "react-router-dom"
 
 
 function TransactionComponent() {
-
     const transactions = useOutletContext();
 
-    console.log(transactions)
+    const transactionalData = transactions.map((transaction) => {
+        return (
+            <tbody>
+                <tr>
+                    <td>{transaction.date}</td>
+                    <td>{transaction.category}</td>
+                    <td>{transaction.description}</td>
+                    <td>{transaction.amount}</td>
+                </tr>
+            </tbody>
+        )
+    })
 
     return (
         <>
             <table>
-                    <thead>
-                        <tr>
-                            <th>DATE</th>
-                            <th>CATEGORY</th>
-                            <th>DESCRIPTION</th>
-                            <th>AMOUNT</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1/1/2024</td>
-                            <td>Income</td>
-                            <td>Paycheck</td>
-                            <td>$2000</td>
-                        </tr>
-                        <tr>
-                            <td>1/7/2024</td>
-                            <td>Expense</td>
-                            <td>Home</td>
-                            <td>$1700</td>
-                        </tr>
-                        <tr>
-                            <td>1/1/2024</td>
-                            <td>Income</td>
-                            <td>Paycheck</td>
-                            <td>$2000</td>
-                        </tr>
-                        <tr>
-                            <td>1/1/2024</td>
-                            <td>Income</td>
-                            <td>Paycheck</td>
-                            <td>$2000</td>
-                        </tr>
-                        <tr>
-                            <td>1/1/2024</td>
-                            <td>Income</td>
-                            <td>Paycheck</td>
-                            <td>$2000</td>
-                        </tr>
-                        <tr>
-                            <td>1/1/2024</td>
-                            <td>Income</td>
-                            <td>Paycheck</td>
-                            <td>$2000</td>
-                        </tr>
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <th>TOTAL SPEND: </th>
-                        </tr>
-                        <tr>
-                            <td>$11,700</td>
-                        </tr>
-                    </tfoot>
-                </table>
+                <thead>
+                    <tr>
+                        <th>DATE</th>
+                        <th>CATEGORY</th>
+                        <th>DESCRIPTION</th>
+                        <th>AMOUNT</th>
+                    </tr>
+                </thead>
+                {transactionalData}
+            </table>
         </>
     )
 }
